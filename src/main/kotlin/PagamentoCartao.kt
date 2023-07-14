4b5bf835ce13e199109238c6b4f8bd3ac9196109
paula.nascimento <paula.nascimento@zup.com.br>
2023-07-14 11:23:12 -0300

    adicionando pagamento por cartão

---
+++ src/main/kotlin/PagamentoCartao.kt
@@ -0,0 +1,8 @@
+import java.math.BigDecimal
+import java.time.LocalDate
+
+class PagamentoCartao(valor: BigDecimal, data: LocalDate, var bandeira :String) : Pagamento(valor, data) {
+    override fun calcularDesconto(): BigDecimal {
+        return valor.multiply(BigDecimal(0.95))
+    }
+}
